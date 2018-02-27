9ccbef179a02d621323730aca33a10c9e1107fdb
kyle verhoog <kyleverhoog@gmail.com>
2018-02-27 17:10:37 -0500

    add push script

--- push.sh
+++ push.sh
@@ -1,1 +1,15 @@
 #! /bin/bash
+SITE_BRANCH=gh-pages
+
+git checkout $SITE_BRANCH
+rm *.html
+git checkout master
+
+racket dth.rkt
+mv today.html index.html # temporary
+
+git checkout $SITE_BRANCH
+git add *.html
+git commit -m "`date`"
+git push origin $SITE_BRANCH
+git checkout master
